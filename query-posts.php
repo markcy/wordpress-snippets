09e1009c9329781605d17f17576d1944641cd827
markcy <mark@bravewhale.com>
2013-06-10 19:13:25 -0700

    Updated query-posts.php

--- query-posts.php
+++ query-posts.php
@@ -1,9 +1,9 @@
-	<?php 
-    	query_posts( 
-    		array(
-    			'post_type'		=> 'bw_homes',
-    			'posts_per_page'	=> 30
-    		)
-    	);
-    	while ( have_posts() ) : the_post(); 
-	?>
+<?php 
+    query_posts( 
+    	array(
+    		'post_type'		=> 'bw_homes',
+    		'posts_per_page'	=> 30
+    	)
+    );
+    while ( have_posts() ) : the_post(); 
+?>
